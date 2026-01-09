3d3b8bdf30b441b56933ba34a34c7e7292c98450
Kanishkar <kanishvijay2005@gmail.com>
2026-01-09 13:08:37 +0530

    test: add test cases for Sketch Order Form

--- jewellery_erpnext/gurukrupa_exports/doctype/sketch_order_form/test_sketch_order_form.py
+++ jewellery_erpnext/gurukrupa_exports/doctype/sketch_order_form/test_sketch_order_form.py
@@ -1,9 +1,191 @@
 # Copyright (c) 2023, Nirali and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
-
+from frappe.utils import now, add_days
+from frappe.model.workflow import apply_workflow
 
 class TestSketchOrderForm(FrappeTestCase):
-	pass
+	def setUp(self):
+		create_test_data()
+
+	def test_sketch_order_created(self):
+		sk_ord_frm = make_sketch_order_form(order_type = 'Sales', design_type = 'New Design')
+
+		sketch_order = frappe.get_all("Sketch Order", filters = {'sketch_order_form': sk_ord_frm.name, 'docstatus': 0})
+		self.assertEqual(len(sketch_order), len(sk_ord_frm.order_details))
+
+	def test_sketch_order_created_mod_design(self):
+		sk_ord_frm = make_sketch_order_form(order_type = 'Sales', design_type = 'Mod', design_code = 'EA00978-003')
+
+		sketch_order = frappe.get_all("Sketch Order", filters = {'sketch_order_form': sk_ord_frm.name, 'docstatus': 0})
+		self.assertEqual(len(sketch_order), len(sk_ord_frm.order_details))
+
+	def test_purchase_order_created(self):
+		sk_ord_frm = make_sketch_order_form(order_type='Purchase', supplier='Test_Supplier', design_type='New Design')
+
+		sketch_order = frappe.get_all("Sketch Order", filters={'sketch_order_form': sk_ord_frm.name, 'docstatus': 0})
+		self.assertEqual(len(sketch_order), len(sk_ord_frm.order_details))
+
+		po = frappe.get_all("Purchase Order", filters={'custom_form_id': sk_ord_frm.name, 'docstatus': 0})
+		self.assertEqual(len(po), 1)
+
+
+def create_test_data():
+	if not frappe.db.exists('Customer', 'Test_Customer_External'):
+		customer = frappe.get_doc(
+			{
+				'doctype': 'Customer',
+				'customer_name': 'Test_Customer_External',
+				'customer_type': 'Individual',
+				'custom_sketch_workflow_state': 'External'
+			}
+		)
+		customer.insert()
+
+	if not frappe.db.exists('Customer', 'Test_Customer_Internal'):
+		customer = frappe.get_doc(
+			{
+				'doctype': 'Customer',
+				'customer_name': 'Test_Customer_Internal',
+				'customer_type': 'Individual',
+				'custom_sketch_workflow_state': 'Internal'
+			}
+		)
+		customer.insert()
+
+	if not frappe.db.exists('Supplier', 'Test_Supplier'):
+		supplier = frappe.get_doc(
+			{
+				'doctype': 'Supplier',
+				'supplier_name': 'Test_Supplier'
+			}
+		)
+		supplier.insert()
+
+	if not frappe.get_value('Department',{'department_name':'Test_Department'},'name'):
+		dep = frappe.get_doc(
+			{
+				'doctype': 'Department',
+				'department_name': "Test_Department",
+				"company": 'Gurukrupa Export Private Limited'
+			}
+		)
+		dep.insert()
+
+	if not frappe.get_value('Branch',{'branch_name':'Test Branch'},'name'):
+		branch = frappe.get_doc(
+			{
+				'doctype': 'Branch',
+				'branch': 'Test Branch',
+				'branch_name': 'Test Branch',
+				'company': 'Gurukrupa Export Private Limited'
+			}
+		)
+		branch.insert()
+
+	if not frappe.db.exists('Sales Person', 'Test_Sales_Person'):
+		salesman = frappe.get_doc(
+			{
+				'doctype': 'Sales Person',
+				'sales_person_name': 'Test_Sales_Person'
+			}
+		)
+		salesman.insert()
+
+
+def make_sketch_order_form(**args):
+	args = frappe._dict(args)
+	sketch_order_form = frappe.new_doc('Sketch Order Form')
+	sketch_order_form.company = 'Gurukrupa Export Private Limited'
+	sketch_order_form.customer_code = 'Test_Customer_External'
+	sketch_order_form.department = frappe.get_value('Department',{'department_name':'Test_Department'},'name')
+	sketch_order_form.branch = frappe.get_value('Branch',{'branch_name':'Test Branch'},'name')
+	sketch_order_form.salesman_name = 'Test_Sales_Person'
+	sketch_order_form.order_type = args.order_type
+	sketch_order_form.order_date = now()
+	sketch_order_form.due_days = 4
+	sketch_order_form.delivery_date = add_days(now(), 4)
+	sketch_order_form.design_by = 'Customer Design'
+	if args.order_type == 'Purchase':
+		sketch_order_form.supplier = args.supplier
+
+	if args.design_type == 'Mod':
+		sketch_order_form.append('order_details', {
+			'design_type': args.design_type,
+			'metal_type': 'Gold',
+			'tag__design_id': args.design_code,
+			'budget': 50000,
+			'metal_target': 1.1,
+			'diamond_target': 1.25,
+			'product_size': '10',
+			'sizer_type': 'Rod',
+			'gemstone_type': 'Ruby',
+			'stone_changeable': 'No',
+			'length': 10,
+			'width': 10,
+			'height': 10,
+			'diamond_part_length': 10,
+			'gemstone_size': '1.60*1.00 MM'
+		})
+
+	else:
+		sketch_order_form.append('order_details', {
+			'design_type': args.design_type,
+			'category': 'Ring',
+			'subcategory': 'God Ring',
+			'setting_type': 'Open',
+			'sub_setting_type1': 'Close-Open Setting',
+			'metal_type': 'Gold',
+			'metal_color': 'Yellow',
+			'metal_touch': '18KT',
+			'budget': 50000,
+			'metal_target': 1.1,
+			'diamond_target': 1.25,
+			'product_size': '10',
+			'sizer_type': 'Rod',
+			'gemstone_type': 'Ruby',
+			'stone_changeable': 'No',
+			'length': 10,
+			'width': 10,
+			'height': 10,
+			'diamond_part_length': 10,
+			'gemstone_size': '1.60*1.00 MM'
+		})
+
+		sketch_order_form.append('order_details', {
+			'design_type': args.design_type,
+			'category': 'Ring',
+			'subcategory': 'God Ring',
+			'setting_type': 'Open',
+			'sub_setting_type1': 'Close-Open Setting',
+			'metal_type': 'Gold',
+			'metal_color': 'Yellow',
+			'metal_touch': '18KT',
+			'budget': 50000,
+			'metal_target': 1.1,
+			'diamond_target': 1.25,
+			'product_size': '10',
+			'sizer_type': 'Rod',
+			'gemstone_type': 'Ruby',
+			'stone_changeable': 'No',
+			'length': 10,
+			'width': 10,
+			'height': 10,
+			'diamond_part_length': 10,
+			'gemstone_size': '1.60*1.00 MM'
+		})
+
+	sketch_order_form.append('age_group',{'design_attribute': '0-12'})
+	sketch_order_form.append('gender',{'design_attribute': 'Kids'})
+	sketch_order_form.append('occasion',{'design_attribute': 'Diwali'})
+	sketch_order_form.append('rhodium',{'design_attribute': 'Black'})
+	sketch_order_form.append('india_states',{'design_attribute': 'Gujarat'})
+
+	sketch_order_form.insert()
+	apply_workflow(sketch_order_form, 'Send For Approval')
+	apply_workflow(sketch_order_form, 'Approve')
+	frappe.db.commit()
+
+	return sketch_order_form
